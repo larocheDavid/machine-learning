dff0a6614b1f538b4a35a75981521cdcd4f3e82e
david <david.laroche@etu.hesge.ch>
2019-10-29 03:02:25 +0100

    "modif"

--- TP2/decision_tree.py
+++ TP2/decision_tree.py
@@ -2,91 +2,6 @@
 
 import numpy as np
 
-'''
-def dataFileToMat(filename):
-	return  np.loadtxt(filename, usecols=(0,1,2,3,4,5,6))
-
-
-def oneAttribToClassMat(dataMat, attribNumber):
-	return dataMat[:, [0, attribNumber]]
-
-
-def countClassPerSection(attribMat, sectionNumber):
-	purClass0, purClass1 = 0, 0
-	impClass0, impClass1 = 0, 0
-
-	for row in attribMat:
-
-		if row[1] <= sectionNumber:
-			
-			if row[0] == 0:
-				purClass0 += 1
-			else:
-				impClass1 += 1
-
-		else:
-			if row[0] == 0:
-				impClass0 += 1
-			else:
-				purClass1 += 1
-
-	return (purClass0, purClass1, impClass0, impClass1) 
-
-
-def gini(results):
-
-	purClass0, purClass1, impClass0, impClass1 = results[0], results[1], results[2], results[3]
-	
-	totalClass0 = purClass0 + impClass0
-	totalClass1 = purClass1 + impClass1
-	
-	gNode1 = 1 - (purClass0/totalClass0)**2 - (impClass0/totalClass0)**2
-	
-	gNode2= 1 - (purClass1/totalClass1)**2 - (impClass1/totalClass1)**2
-	
-	giniPond = (purClass0+impClass0)/(totalClass0+totalClass1)*gNode1 + (purClass1+impClass1)/(totalClass0+totalClass1)*gNode2
-
-	return (gNode1, gNode2, giniPond)
-
-
-def findBestSection(attribMat):
-	
-	bestGeany = 0.5
-	best_section = 1
-	
-	lenAttribute = int(max(attribMat[:,1]))
-	
-	for sectionNumber in range(1, lenAttribute):
-		currGeany = gini(countClassPerSection(attribMat, sectionNumber))
-		print("gini", currGeany)
-	
-		if 	currGeany[2] < bestGeany:
-			bestGeany = currGeany[2]
-			best_section = sectionNumber
-	
-	return (bestGeany, best_section)
-
-
-def findBestAttribute(dataMat):
-	bestGeany = 0.5
-	bestAttrib = 1
-
-	for attribNumber in range(1, len(dataMat[0])):
-		print(attribNumber)
-		attribmat = oneAttribToClassMat(dataMat, attribNumber)		
-		attriBestSection = findBestSection(attribmat)
-
-		if attriBestSection[0]  < bestGeany:
-			bestGeany = attriBestSection[0]
-			bestAttrib = attribNumber
-			bestSection = attriBestSection[1]
-
-	return (bestGeany, bestAttrib, bestSection)
-'''											 
-
-#a = oneAttribToClassMat(dataTrain_1, 1)
-
-#print(findBestAttribute(dataTrain_1))
 
 class Node:
 
@@ -96,6 +11,9 @@ class Node:
 		self.data = data
 		self.maxDepth = maxDepth
 		self.doneAttribute = [False] * 6
+		self.attribute = None
+		self.section = None
+		self.gini = None
 		self.compute()
 
 	def printData(self):
@@ -107,40 +25,38 @@ class Node:
 
 
 	def countClassPerSection(self, attribMat, sectionNumber):
-		purClass0, purClass1 = 0, 0
-		impClass0, impClass1 = 0, 0
+		purClass0, impClass0, purClass1, impClass1 = 0, 0, 0, 0
 
 		for row in attribMat:
-
 			if row[1] <= sectionNumber:
-				
+
 				if row[0] == 0:
 					purClass0 += 1
 				else:
-					impClass1 += 1
-
-			else:
-				if row[0] == 0:
 					impClass0 += 1
-				else:
+			else:
+				if row[0] == 1:
 					purClass1 += 1
+				else:
+					impClass1 += 1
+				
+		return (purClass0, impClass0, purClass1, impClass1) 
 
-		return (purClass0, purClass1, impClass0, impClass1) 
 
+	def computeGini(self, results):
 
-	def gini(self, results):
-
-		purClass0, purClass1, impClass0, impClass1 = results[0], results[1], results[2], results[3]
+		purClass0, impClass0, purClass1, impClass1 = results[0], results[1], results[2], results[3]
 		
 		totalClass0 = purClass0 + impClass0
 		totalClass1 = purClass1 + impClass1
 		
 		gNode1 = 1 - (purClass0/totalClass0)**2 - (impClass0/totalClass0)**2
 		
-		gNode2= 1 - (purClass1/totalClass1)**2 - (impClass1/totalClass1)**2
+		gNode2 = 1 - (purClass1/totalClass1)**2 - (impClass1/totalClass1)**2
 		
-		giniPond = (purClass0+impClass0)/(totalClass0+totalClass1)*gNode1 + (purClass1+impClass1)/(totalClass0+totalClass1)*gNode2
+		giniPond = totalClass0/(totalClass0+totalClass1)*gNode1 + totalClass1/(totalClass0+totalClass1)*gNode2
 
+		#print(purClass0, purClass1)
 		return (gNode1, gNode2, giniPond)
 
 
@@ -152,7 +68,7 @@ class Node:
 		lenAttribute = int(max(attribMat[:,1]))
 		
 		for sectionNumber in range(1, lenAttribute):
-			currGeany = self.gini(self.countClassPerSection(attribMat, sectionNumber))
+			currGeany = self.computeGini(self.countClassPerSection(attribMat, sectionNumber))
 			print("gini", currGeany)
 		
 			if 	currGeany[2] < bestGeany:
@@ -167,25 +83,77 @@ class Node:
 		bestAttrib = 1
 
 		for attribNumber in range(1, len(self.data[0])):
-			print(attribNumber)
-			attribmat = self.oneAttribToClassMat(attribNumber)		
-			attriBestSection = self.findBestSection(attribmat)
+			if self.doneAttribute[attribNumber-1] == False:
+				attribmat = self.oneAttribToClassMat(attribNumber)		
+				attriBestSection = self.findBestSection(attribmat)
 
-			if attriBestSection[0]  < bestGeany:
-				bestGeany = attriBestSection[0]
-				bestAttrib = attribNumber
-				bestSection = attriBestSection[1]
+				if attriBestSection[0]  < bestGeany:
+					bestGeany = attriBestSection[0]
+					bestAttrib = attribNumber
+					bestSection = attriBestSection[1]
 
 		return (bestGeany, bestAttrib, bestSection)
 
+
+	def splitData(self):
+		
+		dataL, dataR = [], []
+
+		for row in self.data:
+			if row[self.attribute] <= self.section:
+				dataL.append(row)		
+			else:
+				dataR.append(row)
+
+		'''
+		purClass0, impClass0, purClass1, impClass1 = 0, 0, 0, 0
+		for row in dataL:
+			if row[0] == 0:
+				purClass0 += 1
+			else:
+				impClass0 += 1
+
+		for row in dataR:
+			if row[0] == 1:
+				purClass1 += 1
+			else:
+				impClass1 += 1
+		#print("GINI", self.computeGini((purClass0, impClass0, purClass1, impClass1)))		
+		'''
+	
+		return (np.array(dataL), np.array(dataR))
+
 	def compute(self):
-		#self.printData()
-		print(self.findBestAttribute())
+		
+		self.gini, self.attribute, self.section = self.findBestAttribute()
+
+		self.doneAttribute[self.attribute-1] = True
+
+		print("Attribute", self.attribute, "Section", self.section, "Gini", self.gini)
+		
+		dataL, dataR = self.splitData()
 
 
+		#print("GINI", self.computeGini((5, 1, 2, 4)))
+		#print(dataR)
+		
+				
 
 
-			
+		if self.maxDepth > 0 and self.gini > 0.1:
+			print("depth", self.maxDepth)
+			if self.left is None and dataL.any():
+				print("anyL", dataL.any())
+				print("dataL", dataL)
+				self.left = Node(dataL, self.maxDepth-1)
+				
+			if self.right is None and dataR.any():
+				print("anyR", dataR.any())
+				print("dataR", dataR)
+				self.right = Node(dataR, self.maxDepth-1)
+		
+		
+	
 def dataFileToMat(filename):
 	return np.loadtxt(filename, usecols=(0,1,2,3,4,5,6))
 
@@ -194,20 +162,4 @@ max_depth = 5
 dataTrain_1 = dataFileToMat("data-20191014/monks-1.train")
 
 three = Node(dataTrain_1, max_depth)
-
-#root = Node(dataTrain_1, max_depth)
-#root.printData()
-
-
-#three.printData()
-
-
-#print(countClassPerSection(a, 2))
-
-#print("GINI", findBestSectionC0_C1(a))
-
-#gin = (5, 2, 1, 4)
-#print("GINI", gini(gin))
-#print(findBestAttribute(dataTrain_1))
-
 
